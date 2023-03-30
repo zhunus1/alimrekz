993c0a6d1ad71ec1cfd46a81df95b7b5c990d6ca
zhunus1 <tengri.07@mail.ru>
2023-03-30 17:25:24 +0600

    changes

--- alimrekz/settings/production.py
+++ alimrekz/settings/production.py
@@ -7,8 +7,12 @@ DEBUG = False
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }

57447d31e7cebca480cb44997b8c0cde861385d8
zhunus1 <tengri.07@mail.ru>
2023-04-17 10:37:15 +0600

    Added cors

--- alimrekz/settings/base.py
+++ alimrekz/settings/base.py
@@ -20,6 +20,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
